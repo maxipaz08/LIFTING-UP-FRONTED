64aedef78f26c9dccd48c47dc399222140d0c8d9
maxipaz08 <pazmax563@gmail.com>
2026-07-14 23:15:07 -0300

    cambios al agregar admins y diseño 2 por que falto cambiar una route

--- src/features/authService.js
+++ src/features/authService.js
@@ -1,6 +1,6 @@
 export const login = async (email, password) => {
   try {
-    const response = await fetch('http://localhost:3001/api/usuarios/login', {
+    const response = await fetch('https://lifting-up-backend.onrender.com/api/usuarios/login', {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json',
